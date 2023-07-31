2a343119cb314f7bc3ef494cb321ef5609c6b49e
stephenliu8964 <dq89_64@hotmail.com>
2023-07-31 15:03:48 +1000

    cleaup of previous week

--- scripts.js
+++ scripts.js
@@ -1,26 +1,20 @@
 const cardList = [{
     title: 'cat 2',
-    path: 'images/cat2.png',
+    path: 'images/kitten-2.png',
     subTitle: 'About Cat 2',
     description: 'Description of Cat 2'
 },
 {
     title: 'cat 3',
-    path: 'images/cat3.png',
+    path: 'images/kitten-3.png',
     subTitle: 'About Cat 3',
     description: 'Description of Cat 3'
-},
-{
-    title: 'cat 4',
-    path: 'images/cat4.png',
-    subTitle: 'About Cat 4',
-    description: 'Description of Cat 4'
 }];
 
 const addCards = (items) => {
     items.forEach(item => {
         let itemToAppend = '<div class="col s4 center-align">'+
-                '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.image+'">'+
+                '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.path+'">'+
                 '</div><div class="card-content">'+
                 '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.link+'</a></p></div>'+
                 '<div class="card-reveal">'+
